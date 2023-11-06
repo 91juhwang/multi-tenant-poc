d7ed405f0f766e1f176b048bda64f49d8f09ff2d
namhee <namhee@namhees-MacBook-Air.local>
2023-11-06 14:12:03 -0500

    Add brandA & brandB pages

---
+++ app/brandB/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function brandB() {
+  return (
+    <main>
+      <h1>Brand AAAA</h1>
+      <h2>
+        <Link href="/" className="text-[#e11d48]">back to home</Link>
+      </h2>
+    </main>
+  );
+}
