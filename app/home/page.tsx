2ee4e8aa5b78670d1f50c69246f43e7498373f8e
James Hwang <91juhwang@gmail.com>
2023-11-08 15:07:21 -0500

    Edit link href to the correct URL

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         </p>
       </div>
       <section className="flex flex-col justify-center pt-12">
-        <Link href="/" className="text-[#e11d48]">Brand A page</Link>
+        <Link href="https://test-app-olive-pi.vercel.app/" className="text-[#e11d48]">Brand A page</Link>
         <Link href="https://test-app-james-hwangs-projects.vercel.app/" className="text-[#e11d48]">brand B page</Link>
       </section>
     </main>
