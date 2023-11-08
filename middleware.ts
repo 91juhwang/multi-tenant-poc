c6b1fd29ebccef1e948d003fdecc71c9ff553c71
James Hwang <91juhwang@gmail.com>
2023-11-08 15:50:01 -0500

    Add matcher config for brands' nested routes

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,12 @@ import { NextRequest, NextResponse } from 'next/server'
 
 // matches to run middleware
 export const config = {
-  matcher: ['/', '/sites/:path*'],
+  matcher: [
+    '/',
+    '/sites/:path*',
+    '/brandA/:path*',
+    '/brandB/:path*'
+  ],
 }
 
 export default async function middleware(req: NextRequest) {
@@ -10,18 +15,21 @@ export default async function middleware(req: NextRequest) {
   const hostName = req.headers.get('host') || ''
 
   // domain to pathname map for each tenant
-  const CLIENT_PATHNAME_MAP:any = {
+  const PARTNER_PATH_MAP:any = {
     'localhost:3000': '/brandA',
     'test-app-olive-pi.vercel.app': '/brandA',
     'test-app-james-hwangs-projects.vercel.app':'/brandB'
   }
 
-  console.log(url.pathname)
+  const pathWOPartner = url.pathname.split('/').splice(2).join('/')
+
   if (url.pathname.startsWith(`/sites`)) {
     url.pathname = `/404`
   } else {
-      url.pathname = `/sites${CLIENT_PATHNAME_MAP[hostName]}`
+    url.pathname = `/sites${PARTNER_PATH_MAP[hostName]}/${pathWOPartner}`
   }
+
+  console.log(url)
 
   return NextResponse.rewrite(url)
 }
